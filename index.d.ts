f57e755e8db21980b0facef61ee564465d9a1a22
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-11-16 12:45:30 +0100

    Define element handle

--- index.d.ts
+++ index.d.ts
@@ -4,14 +4,72 @@
 
 export as namespace BrowserTest
 export interface TestOptions {
+	/**
+   * Maximum duration to run this for, regardless of other timeouts specified on Flood.
+   *
+   * Defaults to `-1` for no timeout.
+   */
 	duration?: number
+
+	/**
+   * Number of times to run this test.
+   * Defaults to `-1` for infinite.
+   */
 	loopCount?: number
 }
 
-export declare function test(options: TestOptions, fn: () => void)
-export declare function step(name: string, fn: (driver: Driver) => Promise<void>)
+export interface StepOptions {
+	/**
+   * Take a screenshot on failure.
+   * @default true
+   */
+	screenshotOnFailure?: boolean
 
-export declare class Driver {
+	/**
+   * Timeout in seconds for all wait and navigation operations.
+   * @default `30` seconds
+   */
+	timeout?: number
+}
+
+/**
+ * `test()` is the entrypoint for creating a new Browser Load Test.
+ *
+ * @export
+ * @param {() => void} fn A callback function to construct the rest of your test.
+ */
+export declare function test(fn: () => void)
+export declare function test(options: TestOptions, fn: () => void)
+
+/**
+ * Declares each step in your test. This must go within the callback from `test()`.
+ *
+ * @export
+ * @param {string} name Step Name
+ * @param {(driver: Driver) => Promise<void>} fn Actual implementation of step
+ */
+export declare function step(name: string, fn: (driver: Driver) => Promise<void>)
+export declare function step(
+	name: string,
+	options: StepOptions,
+	fn: (driver: Driver) => Promise<void>,
+)
+
+declare class Clickable {
+	/**
+     * Sends a click event to the element located at `selector`. If the element is
+     * currently outside the viewport it will first scroll to that element.
+     *
+     * @param {string} selector
+     */
+	public click(selector: string): Promise<void>
+
+	public rightClick(selector: string): Promise<void>
+
+	public middleClick(selector: string): Promise<void>
+}
+
+export declare class Driver extends Clickable {
 	/**
    * Returns a Promise which resolves when the first promise in `...promises` resolves.
    *
@@ -40,18 +98,6 @@ export declare class Driver {
   */
 	public visit(url: string): Promise<void>
 
-	/**
-   * Sends a click event to the element located at `selector`. If the element is
-   * currently outside the viewport it will first scroll to that element.
-   *
-   * @param {string} selector
-   */
-	public click(selector: string): Promise<void>
-
-	public rightClick(selector: string): Promise<void>
-
-	public middleClick(selector: string): Promise<void>
-
 	public selectByValue(selector: string, value: string): Promise<void>
 
 	public selectByIndex(selector: string, index: string): Promise<void>
@@ -73,4 +119,15 @@ export declare class Driver {
 	public waitForElement(selector: string, options: {}): Promise<any>
 
 	public findElementWithText(selector: string, text: string): Promise<any>
+}
+
+declare class ElementHandle extends Clickable {
+	/**
+   * Fetches the value of an attribute on this element
+   *
+   * @param {string} name HTMLElementAttribute
+   * @returns {Promise<string>}
+   * @memberof ElementHandle
+   */
+	attr(name: string): Promise<string>
 }
