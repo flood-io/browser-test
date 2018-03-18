11a82456dda9cc6a3302e91a02b65c6b3673b3eb
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-03-18 13:41:09 -0500

    fix(pencil): Docker invokation

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -11,5 +11,5 @@ command -v docker >/dev/null 2>&1 && {
   docker build -t $DOCKER_IMAGE .
 
   echo "+++ :github: :npm: Release"
-  docker run --rm $DOCKER_IMAGE --env-file .env bash -c "npx semantic-release" && echo OK || exit 1
+  docker run --rm --env-file .env $DOCKER_IMAGE bash -c "npx semantic-release" && echo OK || exit 1
 }
