0c7b8e9fbfd904c6d2d59c097f8effe31c5086d8
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-03-18 13:44:56 -0500

    fix: Adds .env

--- scripts/ci/env.sh
+++ scripts/ci/env.sh
@@ -1,11 +1,10 @@
-env | grep BUILDKITE >> .env
+env | grep BUILDKITE > .env
 env | grep CI >> .env
 env | grep GH_TOKEN >> .env
 env | grep NPM_TOKEN >> .env
 
 echo NODE_ENV=test >> .env
 REVISION=${BUILDKITE_COMMIT:0:8}
-
 DOCKER_IMAGE=floodio/flood-chrome:${REVISION}
 
 echo "--- Print .env"
