425140b04f1b263c2aaa0f07e40fa3c12f0165ce
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-03-18 13:47:32 -0500

    fix: Adds .env

--- scripts/ci/env.sh
+++ scripts/ci/env.sh
@@ -1,11 +1,11 @@
 env | grep BUILDKITE > .env
 env | grep CI >> .env
-env | grep GH_TOKEN >> .env
-env | grep NPM_TOKEN >> .env
+# env | grep GH_TOKEN >> .env
+# env | grep NPM_TOKEN >> .env
 
 echo NODE_ENV=test >> .env
 REVISION=${BUILDKITE_COMMIT:0:8}
-DOCKER_IMAGE=floodio/flood-chrome:${REVISION}
+DOCKER_IMAGE=floodio/flood-chrome:${REVISION:=latest}
 
 echo "--- Print .env"
 cat .env
--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-set -e
+# set -e
 set +x
 
 . $(dirname $0)/env.sh
 
-command -v docker >/dev/null 2>&1 && {
-  [ -f /lib/x86_64-linux-gnu/libdevmapper.so.1.02 ] || ln -s /lib/x86_64-linux-gnu/libdevmapper.so.1.02.1 /lib/x86_64-linux-gnu/libdevmapper.so.1.02 && ldconfig
+echo "~~~ Docker build"
+docker build -t $DOCKER_IMAGE .
 
-  echo "~~~ Docker build"
-  docker build -t $DOCKER_IMAGE .
-
-  echo "+++ :github: :npm: Release"
-  docker run --rm --env-file .env $DOCKER_IMAGE bash -c "npx semantic-release" && echo OK || exit 1
-}
+echo "+++ :github: :npm: Release"
+docker run --rm --env-file .env $DOCKER_IMAGE npx semantic-release && echo OK || exit 1
+# docker run --rm --env-file .env floodio/dashboard:${REVISION} ember deploy ${TARGET} ${ACTIVATE} --verbose
